789d918b0db799adbdd026e5ff926a1eda95a174
HimanshuSwami <96292598+HimanshuSwami-Developer@users.noreply.github.com>
2024-05-07 13:59:38 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
-const uri='mongodb+srv://Himanshu:gokuss3@oyebusy.aycjwoy.mongodb.net/Oyebusy'
+const uri='mongodb+srv://Himanshu:gokuss3@cluster0.nyxl32d.mongodb.net/memo'
+    // 'mongodb+srv://Himanshu:gokuss3@oyebusy.aycjwoy.mongodb.net/Oyebusy'
+// mongodb+srv://Himanshu:<password>@cluster0.nyxl32d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 const connectMongo=()=>{
     mongoose.connect(uri,()=>{
         console.log("connected to mongo");
